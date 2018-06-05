acf68ad45522ca88bca8015572bfc9f7d4933f35
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-06-05 18:28:46 +0100

    not really working but meh

--- angular-src/src/app/services/list.service.ts
+++ angular-src/src/app/services/list.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+/*list.service.ts*/
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http';
+
+import { List } from '../models/List'
+import { Observable } from 'rxjs';
+import { map, filter, catchError, mergeMap } from 'rxjs/operators';
+
+
+@Injectable()
 export class ListService {
 
-  constructor() { }
+    constructor(private http: Http) { }
+
+    private serverApi= 'http://localhost:3000';
+
+    public getAllLists():Observable<List[]> {
+
+        let URI = `${this.serverApi}/bucketlist/`;
+        return this.http.get(URI)
+        .pipe(
+            map((res) => res.json())
+        );
+    }
+
+    // public deleteList(listId : string) {
+    //   let URI = `${this.serverApi}/bucketlist/${listId}`;
+    //     let headers = new Headers;
+    //     headers.append('Content-Type', 'application/json');
+    //     return this.http.delete(URI, {headers})
+    //     .map(res => res.json());
+    // }
 }
